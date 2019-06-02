b005a1d6f2818471f35292f1116d92035f134a65
Q.McKinley <q.mckinley@outlook.com>
2019-06-01 21:00:29 -0400

    accessing api from browser

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const express = require('express');
 const hbs = require('hbs')
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
 
 const app = express();
 
@@ -22,28 +24,45 @@ app.get('/', (req, res) => {
   res.render('index', {
     title: 'Weather App',
     name: 'Q.McKinley'
-  })
-})
+  });
+});
 
 app.get('/about', (req, res) => {
   res.render('about', {
     title: 'About Me',
     name: 'Q.McKinley'
-  })
-})
+  });
+});
 
 app.get('/help', (req, res) => {
   res.render('help', {
     title: 'Help Page',
     name: 'Q.McKinley',
     message: 'This is an example help message'
-  })
-})
+  });
+});
 
 app.get('/weather', (req, res) => {
-  res.send({
-    location: 'New York',
-    forecast: 'Partly Cloudy'
+  if (!req.query.address) {
+    return res.send({
+      error: 'You must provide a search address'
+    });
+  };
+  const { address } = req.query;
+  geocode(address, (err, { longitude, latitude, location } = {}) => {
+    if (err) {
+      return res.send({ error: err });
+    };
+    forecast(longitude, latitude, (error, forecastData) => {
+      if (error) {
+        return res.send({ error: error });
+      };
+      res.send({
+        location,
+        forecast: forecastData,
+        address
+      });
+    });
   });
 });
 
@@ -52,18 +71,18 @@ app.get('/help/*', (req, res) => {
     title: '404 Response',
     name: 'Q.McKinley',
     message: 'Help article not found.'
-  })
-})
+  });
+});
 
 app.get('*', (req, res) => {
   res.render('404', {
     title: '404 Response',
     name: 'Q.McKinley',
     message: 'Page Not Found'
-  })
-})
+  });
+});
 
 //
 app.listen(3000, () => {
-  console.log('Server is up on port 3000.')
+  console.log('Server is up on port 3000.');
 });
